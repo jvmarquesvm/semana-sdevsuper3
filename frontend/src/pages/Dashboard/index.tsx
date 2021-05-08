3f712d71b276bdd7a63bb716961e72fc57f2a1e7
Joao Victor <joaovictor2@hotmail.com>
2021-05-08 15:13:56 -0300

    #12 - FrontEnd Configurando Rotas

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,38 @@
+import ImgDsDark from 'assets/img/ds_dark.svg';
+import BarChart from 'components/BarChart';
+import DataTable from 'components/DataTable';
+import DonutChart from 'components/DonutsChart';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+
+const Dashboard = () => {
+    return (
+        <div >
+           <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary">DashBoard de Vendas</h1>
+                <div className = "row px-3 ">
+                <div className = "col-sm-6">
+                    <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                    <BarChart />
+                </div>
+                <div className = "col-sm-6">
+                    <h5 className="text-center text-secondary">Todas as Vendas</h5>
+                    <DonutChart />
+                </div>  
+                </div>
+
+                <div className="py-3">
+                <h2 className="text-primary"> Todas Vendas </h2>
+                </div>
+
+                <DataTable />
+            </div>
+            <Footer />
+            </>
+        </div>
+    )
+}
+
+export default Dashboard;
